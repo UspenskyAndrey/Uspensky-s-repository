bdb1d80746487c5f0d5982fa566ff02046cf75d5
UspenskyAndrey <m.andreyuspensky13@yandex.ru>
2019-03-16 00:04:22 +0300

    Create task1.js

---
+++ task2/task1.js
@@ -0,0 +1,10 @@
+var x = function(a){
+ if (typeof(a) == 'number')
+  console.log(typeof(a))
+ else {
+ if (typeof(a)=='string')
+  console.log(typeof(a))
+  else 
+  console.log('udefined')
+ } 
+}
