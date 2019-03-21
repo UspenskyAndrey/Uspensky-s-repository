bf8a08784457da276892c5c348c948b89f402027
UspenskyAndrey <m.andreyuspensky13@yandex.ru>
2019-03-21 16:16:54 +0300

    Create task1-2.js

---
+++ classwork/classwork 15.03/task1-2.js
@@ -0,0 +1,13 @@
+var workWithUser =    // прототип для работы с пользователями
+{
+   getAge: function() {},
+   getName: function() {},
+   changeName: function(name) {},
+   changeAge: function(age) {}   
+}
+var workWithEmployee =   // прототив для работы с работниками
+{
+  getJobTitle: function() {},
+  getSalary: function() {},
+  promote: function(jobTitle, salary){}
+}
