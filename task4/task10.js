165a5b180e56544d0d096fda74dc986f8272eb42
UspenskyAndrey <m.andreyuspensky13@yandex.ru>
2019-03-25 22:51:40 +0300

    Delete task10.js

--- task4/task10.js
+++
@@ -1,8 +0,0 @@
-var strReverse = function(a) {
-  var str = new String(a),
-      str1 = new String();
-  for (var i = str.length - 1; i > -1; --i) {
-    str1 += str[i];
-  }
-  return str1.toString();
-}
