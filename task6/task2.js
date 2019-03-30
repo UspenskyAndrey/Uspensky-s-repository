d8d0f63a05e30fc676118d1dbef31d346ecf43e9
UspenskyAndrey <m.andreyuspensky13@yandex.ru>
2019-03-31 01:01:39 +0300

    Create task2.js

---
+++ task6/task2.js
@@ -0,0 +1,8 @@
+var anotherSome = function(array, callback) {
+  for (var i = 0; i < array.length; ++i) {
+    if (callback(array[i], i, array)) {
+      return true;
+    }
+  }
+  return false;
+}
