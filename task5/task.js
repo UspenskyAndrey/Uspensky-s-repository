b118e3d8036ac8a0d17018c0ec391a0cb1ae99fb
UspenskyAndrey <m.andreyuspensky13@yandex.ru>
2019-04-03 12:28:38 +0300

    Update task.js

--- task5/task.js
+++ task5/task.js
@@ -5,39 +5,31 @@ return {
     return result;
   },
   reset: function() {
-    return result  = 0;
+    return result = 0;
   },
   add: function add(val) {
     if (!isNaN(val)) {
       result += val;
-      return add;
-    } else {
-      return add;
     }
+    return add; 
   },
   substract: function sub(val) {
     if (!isNaN(val)) {
       result -= val;
-      return sub;
-    } else {
-      return sub;
     }
+    return sub;
   },
   divide: function div(val) { 
     if ((!isNaN(val)) && (val !==0)) {
      result /= val;
-     return div;
-     } else {
-       return div;
      }
+     return div;
   },
   multiply: function mul(val) {
     if (!isNaN(val)) {
      result *= val;
-     return mul;
-     } else {
-       return mul;
      }
+     return mul;
   },
 
 }})()
