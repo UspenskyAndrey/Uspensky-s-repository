489446bc7aa6ce2eedd6fbef755dde038aef92cb
UspenskyAndrey <m.andreyuspensky13@yandex.ru>
2019-04-03 12:44:51 +0300

    Update task5.js

--- task6/task5.js
+++ task6/task5.js
@@ -1,7 +1,7 @@
 var anotherMap = function(array, callback) {
   var newArr = [];
-  for (var i = 0, j = 0; i < array.length; ++i, ++j) {
-    newArr[j] = callback(array[i], i, array);
+  for (var i = 0; i < array.length; ++i) {
+    newArr.push(callback(array[i], i, array));
   }
   return newArr;
 }
